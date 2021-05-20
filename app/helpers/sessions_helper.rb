542734b6fc343263b512c889aff355e74eb49823
Ocean <oosheng.soo99@gmail.com>
2021-05-20 13:51:28 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,7 +20,7 @@ module SessionsHelper
     if (user_id = session[:user_id])
       user = User.find_by(id: user_id)
       if user && session[:session_token] == user.session_token
-        @current_user ||= user if session[:session_token] == user.session_token
+        @current_user = user
       end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
@@ -29,6 +29,11 @@ module SessionsHelper
         @current_user = user
       end
     end
+  end
+  
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user && user == current_user
   end
   
   #Forgets a persistent session.
@@ -50,7 +55,10 @@ module SessionsHelper
     @current_user = nil
   end
   
-  
+  #Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
   
   
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
   
   test "layout links" do
     get root_path
@@ -9,8 +12,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-    get signup_path
-    assert_select "title", full_title("Sign up")
+    assert_select "a[href=?]", login_path
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
   end
   
 end
