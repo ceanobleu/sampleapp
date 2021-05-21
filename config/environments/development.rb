fdf38ce648c80b56318b3d86b75377691bdfdc2a
Ocean <oosheng.soo99@gmail.com>
2021-05-21 17:01:03 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,12 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  host = '0668e90fc57d4407b9b8c9a87fe31a9f.vfs.cloud9.us-east-2.amazonaws.com'
+  # Use this on the cloud IDE.
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  # Use this if developing on localhost.
+  # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   config.action_mailer.perform_caching = false
 
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -2,38 +2,60 @@ require "test_helper"
 
 class UsersEditTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:michael)
+    @admin = users(:michael)
+    @non_admin = users(:archer)
+  end
+  
+  test "index as admin including pagination and delete links" do
+    log_in_as(@admin)
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.first.toggle!(:activated)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    
+    assigns(:users).each do |user|
+      assert user.activated?
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
+    end
+    
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
   end
   
   test "unsuccessful edit" do
-    log_in_as(@user)
-    get edit_user_path(@user)
+    log_in_as(@non_admin)
+    get edit_user_path(@non_admin)
     assert_template 'users/edit'
-    patch user_path(@user), params: { user: { name: "",
-                                      email: "foo@invalid",
-                                      password: "foo",
-                                      password_confirmation: "bar" } }
+    patch user_path(@non_admin), params: { user: { name: "",
+                                              email: "foo@invalid",
+                                              password: "foo",
+                                              password_confirmation: "bar" } }
     assert_template 'users/edit'
     assert_select "div.alert", text: "The form contains 4 errors."
   end
   
   test "successful edit with friendly forwarding" do
-    get edit_user_path(@user)
+    get edit_user_path(@non_admin)
     assert_not_nil session[:forwarding_url]
-    log_in_as(@user)
-    assert_redirected_to edit_user_path(@user)
+    log_in_as(@non_admin)
+    assert_redirected_to edit_user_path(@non_admin)
     name  = "Foo Bar"
     email = "foo@bar.com"
-    patch user_path(@user), params: { user: { name:  name,
+    patch user_path(@non_admin), params: { user: { name:  name,
                                               email: email,
                                               password:              "foobar",
                                               password_confirmation: "foobar" } }
     assert_not flash.empty?
-    assert_redirected_to @user
+    assert_redirected_to @non_admin
     assert_nil session[:forwarding_url]
-    @user.reload
-    assert_equal @user.name,  name
-    assert_equal @user.email, email
+    @non_admin.reload
+    assert_equal @non_admin.name,  name
+    assert_equal @non_admin.email, email
   end
   
 end
